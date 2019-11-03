dce328a74e7ab9f772dfcec265c9c016c2238546
Chris Mikstas <chrismikstas@gmail.com>
2019-11-03 13:43:31 -0700

    Established MySQL connection on js file.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,21 @@
+/** Require Variables **/
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+/** Global Variables **/
+
+/** MySQL Connection **/
+var connection = mysql.createConnection(
+{
+    host: "localhost",
+    
+    // Your port; if not 3306
+    port: 3306,
+    
+    // Your username
+    user: "root",
+    
+    // Your password and database that is being accessed
+    password: "root",
+    database: "greatbay_db"
+});
